6a62c647fd136ffd5a29349824241c97148ecb73
Muhammad Fathan Fauzan <muhammadfatanfauzan@gmail.com>
2023-09-27 15:17:02 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+];
