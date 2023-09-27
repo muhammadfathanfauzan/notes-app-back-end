36cd7d98dcc0c01f9d161a17d5255d83347b07dc
Muhammad Fathan Fauzan <muhammadfatanfauzan@gmail.com>
2023-09-27 23:33:43 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
-
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
